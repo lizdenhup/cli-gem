5282bf27919233655ddef10d7a2a8aa856834c3b
lizdenhup <lizdenhup@gmail.com>
2016-06-19 18:58:41 -0500

    openssl error

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
-
+require_relative './story.rb'
 class Scraper
 
   def self.scrape_front_page(url)
@@ -9,14 +9,18 @@ class Scraper
     front_page.encoding = 'utf-8'
     stories = []
 
-    story_item = front_page.css('l_title + a')
+    story_item = front_page.css('.l_box')
     story_item.each do |item|
-      title = story_item.text 
-       story = {title: title}
+      # binding.pry 
+      title = item.css('.l_title a').text
+      link = 'https://en.wikinews.org' + item.css('.l_title a').first.attributes['href'].value
+      summary = item.css('.l_summary').first.children.text
+
+      story = Story.new.tap { |s| s.title = title; s.link = link; s.summary = summary}
       stories << story if story
-      binding.pry 
       end 
       stories 
+      binding.pry
   end
 
 
